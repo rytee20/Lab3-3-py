2354867c88237f5a2356127fd9333bf93abf91e0
Ekaterina <94058290+rytee20@users.noreply.github.com>
2022-10-24 22:21:30 +0700

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,25 +1,26 @@
-def hofstadter_f_m(n: int) -> tuple[int, int]:  # функция сокращения дроби
-    if n ==0:
+def hofstadter_f_m(n: int) -> tuple[int, int]:  # функция-генератор
+    if n == 0:
         yield
     else:
-        f = [1]
+        f = [1]  # первые элементы последовательностей
         m = [0]
-        for i in range(1,n):
-            m.append(i-f[m[i-1]])
+        for i in range(1, n):  # в зависимости от передаваемого количества
+            m.append(i-f[m[i-1]])  # вычисляем элементы по формуле
             f.append(i - m[f[i - 1]])
-        for i in range(0, n):
+        for i in range(0, n):  # возвращаем элементы
             yield f[i], m[i]
 
 
-n = 0  #
+n = 0  # номер последовательности
 
 while True:  # ввод  с проверкой
         try:
+            n = int(input("Введите номер последовательностей: "))
             while n <= 0:
-                n = int(input("Введите: "))
+                n = int(input("Вы ввели не верно. Попробуйте снова: \nВведите номер последовательностей: "))
             break
         except ValueError:
             print("Вы ввели не верно. Попробуйте снова: ")
 
-for f_m in hofstadter_f_m(n):
-    print(f_m, end=', ')
+for f_m in hofstadter_f_m(n):  # вывод
+    print(f_m, end=' ')
